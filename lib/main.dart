26672f9cc048baa5f7260491578672b4441c04ed
linlian <linlian@magefitness.com>
2020-02-19 14:31:39 +0800

    添加第一栏

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,59 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: <Widget>[
+        Expanded(
+          //占据剩余空间
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  'Oeschinen Lake Compgroud',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              )
+            ],
+          ),
+        ),
+        Icon(
+          Icons.star,
+          color: Colors.red[500],
+        ),
+        Text('41'),
+      ],
+    ),
+  );
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+//      debugShowMaterialGrid: true,
+
       title: 'Flutter Demo Test',
-      home:Scaffold(
+      home: Scaffold(
         appBar: AppBar(
-          title:Text("title text"),
+          title: Text("title text"),
         ),
-        body: Center(
-          child: Text("body text"),
+        body: Column(
+          children: <Widget>[
+            titleSection,
+          ],
         ),
       ),
     );
   }
 }
-
