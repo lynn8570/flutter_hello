c7624a77a468c63c4e461b791d7f45deca1ac376
linlian <linlian@magefitness.com>
2020-02-19 15:03:19 +0800

    添加按钮和内容区域

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,35 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Color color = Theme
+        .of(context)
+        .primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColumn(color, Icons.call, 'Call'),
+          _buildButtonColumn(color, Icons.near_me, 'Route'),
+          _buildButtonColumn(color, Icons.call, 'Share'),
+        ],
+      ),
+    );
+
+
+    Widget textSection = Container(padding: const EdgeInsets.all(32),
+      child: Text(
+        'dsaljdljalsjljlfjldjlsfjldjslfjlsdjfldsjflsjdlfjl,文章的内容区域，'
+            'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
 //      debugShowMaterialGrid: true,
 
@@ -53,9 +82,36 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: <Widget>[
             titleSection,
+            buttonSection,
+            textSection,
           ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    //上面是ICON,下面是text 的一个column
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          color: color,
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color
+            ),
+          ),
+        )
+      ],
     );
   }
 }
