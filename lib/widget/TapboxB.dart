48a0fdd3f4f2293379fc6318a533e1575ff52ee9
linlian <linlian@magefitness.com>
2020-02-20 17:57:01 +0800

    formate

--- lib/widget/TapboxB.dart
+++ lib/widget/TapboxB.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 
-
 class TapboxB extends StatelessWidget {
-
-
   TapboxB({Key key, this.active: false, @required this.onChanged});
 
   final bool active;
   final ValueChanged<bool> onChanged;
 
-
-  void _handleTap(){
+  void _handleTap() {
     onChanged(!active);
   }
 
@@ -21,19 +17,15 @@ class TapboxB extends StatelessWidget {
       child: Container(
         child: Center(
           child: Text(
-            active?'Active':'Inactive',
-            style: TextStyle(fontSize: 32.0,color: Colors.white),
+            active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
           ),
         ),
         width: 200.0,
         height: 200.0,
         decoration: BoxDecoration(
-          color: active?Colors.lightGreen[700]:Colors.grey[600]
-        ),
+            color: active ? Colors.lightGreen[700] : Colors.grey[600]),
       ),
     );
   }
-
-
-
 }
